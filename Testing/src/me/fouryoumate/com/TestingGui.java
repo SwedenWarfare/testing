8332a524565f7d67a09abf0f7979e0c81fbd4891
elev <elev@CND8261HL5.learnet.se>
2020-08-04 16:50:07 +0200

    re

--- Testing/src/me/fouryoumate/com/TestingGui.java
+++ Testing/src/me/fouryoumate/com/TestingGui.java
@@ -7,7 +7,13 @@ import javax.swing.JFrame;
 public class TestingGui extends JFrame{
 	JFrame frame = new JFrame();
 	public TestingGui() {
-		
+		 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		 //Set JFrame size
+		 setSize(500,500);
+
+		 //Make JFrame visible
+		 setVisible(true);
 	}
 	public void paint (Graphics g) {
 		
