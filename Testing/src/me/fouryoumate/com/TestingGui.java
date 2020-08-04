4846e9d2f4236e555a3e993029f276b48abcaa66
elev <elev@CND8261HL5.learnet.se>
2020-08-04 16:48:02 +0200

    re

--- Testing/src/me/fouryoumate/com/TestingGui.java
+++ Testing/src/me/fouryoumate/com/TestingGui.java
@@ -1,10 +1,15 @@
 package me.fouryoumate.com;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class TestingGui extends JFrame{
 	JFrame frame = new JFrame();
 	public TestingGui() {
+		
+	}
+	public void paint (Graphics g) {
 		
 	}
 }
